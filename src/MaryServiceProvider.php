f92325b9ffbe85e3f4a2fd0c27fe7933943c7a93
Essa Mamdani <mamdani.info@gmail.com>
2023-09-15 16:14:32 +0500

    Update MaryServiceProvider.php
    Add Support Of Textarea

--- src/MaryServiceProvider.php
+++ src/MaryServiceProvider.php
@@ -34,6 +34,7 @@ use Mary\View\Components\Stat;
 use Mary\View\Components\Tab;
 use Mary\View\Components\Table;
 use Mary\View\Components\Tabs;
+use Mary\View\Components\Textarea;
 use Mary\View\Components\TimelineItem;
 use Mary\View\Components\Toast;
 use Mary\View\Components\Toggle;
@@ -90,6 +91,7 @@ class MaryServiceProvider extends ServiceProvider
         Blade::component('table', Table::class);
         Blade::component('tab', Tab::class);
         Blade::component('tabs', Tabs::class);
+        Blade::component('textarea', Textarea::class);
         Blade::component('timeline-item', TimelineItem::class);
         Blade::component('toast', Toast::class);
         Blade::component('toggle', Toggle::class);
