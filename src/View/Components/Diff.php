dd6576c04402b9ced857d2bb43f8ae9941dc8e48
Robson Tenório <rrtenorio@gmail.com>
2023-09-17 19:36:20 -0300

    Diff: fix missing closing `</div>`

--- src/View/Components/Diff.php
+++ src/View/Components/Diff.php
@@ -46,6 +46,7 @@ class Diff extends Component
         return <<<'HTML'
             <div id="differ-{{ $uuid }}" 
                  class="[&_.d2h-diff-table]:text-xs [&_.d2h-file-header]:bg-base-100 [&_.d2h-file-wrapper]:border-dashed [&_.d2h-file-wrapper]:border [&_.d2h-file-wrapper]:bg-base-100 [&_.d2h-del]:bg-red-50 [&_.d2h-ins]:bg-green-50 [&_.d2h-code-line-ctn]:whitespace-pre-wrap [&_.d2h-code-side-line]:w-auto">                    
+            </div>
                 
             <script>
                 var targetElement = document.getElementById('differ-{{ $uuid }}');                
