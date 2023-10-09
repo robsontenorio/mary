61c610ddf3d424c9048fb96aaedb88b5156e2b42
Robson Tenório <rrtenorio@gmail.com>
2023-10-09 14:08:18 -0300

    Fix merge

--- src/View/Components/Header.php
+++ src/View/Components/Header.php
@@ -4,20 +4,25 @@ namespace Mary\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 
 class Header extends Component
 {
+    public string $anchor = '';
+
     public function __construct(
         public ?string $title = null,
         public ?string $subtitle = null,
         public ?bool $separator = false,
+        public ?bool $withAnchor = false,
         public ?string $size = 'text-4xl',
 
         // Slots
         public mixed $middle = null,
         public mixed $actions = null,
     ) {
+        $this->anchor = Str::slug($title);
     }
 
     public function render(): View|Closure|string
@@ -26,7 +31,18 @@ class Header extends Component
                 <div {{ $attributes->class(["mb-10"]) }}>
                     <div class="flex flex-wrap gap-5 justify-between items-center">
                         <div>
-                            <div class="{{$size}} font-extrabold">{{ $title }}</div>
+                            <div class="{{$size}} font-extrabold mary-header-anchor" id="{{ $anchor }}">
+                                @if($withAnchor)
+                                    <a href="#{{ $anchor }}">
+                                @endif
+
+                                {{ $title }}
+
+                                @if($withAnchor)
+                                    </a>
+                                @endif
+                            </div>
+
                             @if($subtitle)
                                 <div class="text-gray-500 text-sm mt-1">{{ $subtitle }}</div>
                             @endif
