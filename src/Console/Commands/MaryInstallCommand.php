28e917dab04dbe6d0838842382635adce614aa80
Robson Tenório <rrtenorio@gmail.com>
2023-10-11 17:00:31 -0300

    Installer: improve with validations
    - Check for starter kits
    - Check for composer.json and package.json
    - Check for stubs

--- src/Console/Commands/MaryInstallCommand.php
+++ src/Console/Commands/MaryInstallCommand.php
@@ -2,8 +2,11 @@
 
 namespace Mary\Console\Commands;
 
+use Arr;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Str;
 
 class MaryInstallCommand extends Command
 {
@@ -13,34 +16,112 @@ class MaryInstallCommand extends Command
 
     public function handle()
     {
-        $this->info("\n\n🔨 Installing Mary...\n\n");
+        $this->info("🔨 Installing Mary...\n");
 
-        $directories = Process::tty()->run('mkdir -p app/Livewire && mkdir -p resources/views/components/layouts');
-        $this->info($directories->output());
+        /**
+         * Check for starter kits
+         */
+        $composerJson = File::get(base_path() . "/composer.json");
+        $targets = ['breeze', 'jetstream', 'genesis'];
 
-        $livewire = Process::tty()->run('composer require livewire/livewire');
-        $this->info($livewire->output());
+        if (Str::of($composerJson)->contains($targets)) {
+            $this->error("Automatic install works only for brand-new Laravel projects.");
+            $this->warn("Detected one of these: " . Arr::join($targets, ', ', ' or '));
 
-        $js = Process::tty()->run('yarn add -D tailwindcss daisyui@latest postcss autoprefixer && npx tailwindcss init -p');
-        $this->info($js->output());
+            return;
+        }
 
-        $layout = Process::tty()->run('cp '.__DIR__.'/../../../stubs/app.blade.php resources/views/components/layouts/');
-        $this->info($layout->output());
+        /**
+         * Check for JS packages
+         */
+        $packageJson = File::get(base_path() . "/package.json");
+        $targets = ['tailwindcss', 'daisyui'];
 
-        $css = Process::tty()->run('cp '.__DIR__.'/../../../stubs/app.css resources/css/');
-        $this->info($css->output());
+        if (Str::of($packageJson)->contains($targets)) {
+            $this->error("Automatic install works only for brand-new Laravel projects.");
+            $this->warn("Detected one of these: " . Arr::join($targets, ', ', ' or '));
 
-        $tailwindConfig = Process::tty()->run('cp '.__DIR__.'/../../../stubs/tailwind.config.js .');
-        $this->info($tailwindConfig->output());
+            return;
+        }
 
-        $welcome = Process::tty()->run('cp '.__DIR__.'/../../../stubs/Welcome.php app/Livewire/');
-        $this->info($welcome->output());
+        /**
+         * Check for all stubs
+         */
+        collect([
+            [
+                'path' => 'resources/views/components/layouts',
+                'name' => 'app.blade.php'
+            ],
+            [
+                'path' => 'resources/css/',
+                'name' => 'app.css'
+            ],
+            [
+                'path' => 'app/Livewire/',
+                'name' => 'Welcome.php',
+            ],
+            [
+                'path' => '',
+                'name' => 'tailwind.config.js',
+            ]
+        ])->each(function (array $item) {
+            $file = base_path() . '/' . $item['path'] . $item['name'];
 
-        $route = Process::tty()->run('cp '.__DIR__.'/../../../stubs/web.php routes/');
-        $this->info($route->output());
+            if (File::exists($file)) {
+                $this->error("Automatic install works only for brand-new Laravel projects.");
+                $this->warn('Detected:' . $file);
 
-        $this->info('🌟 Done!');
-        $this->info("\n\n==> Run `yarn dev`\n\n");
+                exit;
+            }
+        });
 
+        /**
+         * Install Livewire
+         */
+        $this->info("\nInstalling Livewire...\n");
+
+        Process::run('composer require livewire/livewire', function (string $type, string $output) {
+            echo $output;
+        })->throw();
+
+        /**
+         * Install daisyUI + Tailwind
+         */
+        $this->info("\nInstalling daisyUI + Tailwind...\n");
+
+        Process::run('yarn add -D tailwindcss daisyui@latest postcss autoprefixer && npx tailwindcss init -p', function (string $type, string $output) {
+            echo $output;
+        })->throw();
+
+        /**
+         * Copy all stubs
+         */
+        $this->info("Copying stubs...\n");
+
+        Process::run('mkdir -p app/Livewire && mkdir -p resources/views/components/layouts', function (string $type, string $output) {
+            echo $output;
+        })->throw();
+
+        Process::run('cp ' . __DIR__ . '/../../../stubs/app.blade.php resources/views/components/layouts/', function (string $type, string $output) {
+            echo $output;
+        })->throw();
+
+        Process::run('cp ' . __DIR__ . '/../../../stubs/app.css resources/css/', function (string $type, string $output) {
+            echo $output;
+        })->throw();
+
+        Process::run('cp ' . __DIR__ . '/../../../stubs/tailwind.config.js .', function (string $type, string $output) {
+            echo $output;
+        })->throw();
+
+        Process::run('cp ' . __DIR__ . '/../../../stubs/Welcome.php app/Livewire/', function (string $type, string $output) {
+            echo $output;
+        })->throw();
+
+        Process::run('cp ' . __DIR__ . '/../../../stubs/web.php routes/', function (string $type, string $output) {
+            echo $output;
+        })->throw();
+
+        $this->info("\n🌟 Done! Run `yarn dev or npm run dev`\n");
     }
 }
