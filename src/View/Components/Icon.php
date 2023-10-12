f475135492cef21922d5d32ebe5e5f7912612910
Robson Tenório <rrtenorio@gmail.com>
2023-10-12 11:40:52 -0300

    Icon: allow another iconset

--- src/View/Components/Icon.php
+++ src/View/Components/Icon.php
@@ -4,6 +4,7 @@ namespace Mary\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 
 class Icon extends Component
@@ -11,27 +12,29 @@ class Icon extends Component
     public string $uuid;
 
     public function __construct(
-        public string $name,
+        public string $name
     ) {
         $this->uuid = md5(serialize($this));
     }
 
     public function icon(): string
     {
-        return "heroicon-{$this->name}";
+        $name = Str::of($this->name);
+
+        return $name->contains('.') ? $name->replace('.', '-') : "heroicon-{$this->name}";
     }
 
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                <x-svg 
-                    :name="$icon()" 
-                    {{ 
+                <x-svg
+                    :name="$icon()"
+                    {{
                         $attributes->class([
                             'inline',
                             'w-5 h-5' => !Str::contains($attributes->get('class'), ['w-', 'h-'])
-                        ]) 
-                     }} 
+                        ])
+                     }}
                 />
             HTML;
     }
