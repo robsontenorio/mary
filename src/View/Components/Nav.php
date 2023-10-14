7c3b20bc34e4b64c9437945b8d0216c7b0982157
Robson Tenório <rrtenorio@gmail.com>
2023-10-14 00:30:00 -0300

    Nav: fix `actions` slot gap

--- src/View/Components/Nav.php
+++ src/View/Components/Nav.php
@@ -22,23 +22,23 @@ class Nav extends Component
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                @if($sticky) 
-                    <div class="sticky top-0 z-10"> 
+                @if($sticky)
+                    <div class="sticky top-0 z-10">
                 @endif
 
                     <header {{ $attributes->class(["bg-base-100 border-gray-100 border-b"]) }}>
                         <div class="mx-auto px-6 py-5 flex items-center @if(!$fullWidth) max-w-screen-2xl @endif">
-                            <div {{ $attributes->class(["flex-1 flex items-center"]) }}>                        
-                                {{ $brand }}                        
+                            <div {{ $attributes->class(["flex-1 flex items-center"]) }}>
+                                {{ $brand }}
                             </div>
-                            <div {{ $attributes->class(["flex items-center gap-4 lg:gap-8"]) }}>
+                            <div {{ $attributes->class(["flex items-center gap-4"]) }}>
                                 {{ $actions }}
                             </div>
                         </div>
                     </header>
-                    
-                @if($sticky) 
-                    </div>                
+
+                @if($sticky)
+                    </div>
                 @endif
                 HTML;
     }
