761383fe4c0abe9abbe744956bd337c41226f8a2
Robson Tenório <rrtenorio@gmail.com>
2023-11-04 23:34:55 -0300

    Draft: Introduce component renaming

--- config/mary.php
+++ config/mary.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    //
+    /**
+     * Default is empty.
+     *    prefix => ''
+     *              <x-button />
+     *              <x-card />
+     *
+     * Renaming all components:
+     *    prefix => 'mary-'
+     *               <x-mary-button />
+     *               <x-mary-card />
+     */
+    'prefix' => ''
 ];
+
