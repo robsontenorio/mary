44af517366988fdc773a3f6fb39410edfabb85a5
Robson Tenório <rrtenorio@gmail.com>
2023-11-04 23:32:40 -0300

    Fix: sidebar collapse + remove unused assets (#105)
    - Cache were global. Session is per user.
    - Remove unused assets.

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
 
-Route::get('/mary/toogle-sidebar', function (Request $request) {
+Route::middleware('web')->get('/mary/toogle-sidebar', function (Request $request) {
     if ($request->collapsed) {
-        cache(['mary-sidebar-collapsed' => $request->collapsed]);
+        session(['mary-sidebar-collapsed' => $request->collapsed]);
     }
-});
-
-Route::middleware('cache.headers:public;max_age=2628000;etag')->get('/mary/asset', function (Request $request) {
-    if (! $request->name) {
-        abort(404);
-    }
-
-    if (Str::of($request->name)->contains('..')) {
-        abort(404);
-    }
-
-    $file = Str::of($request->name)->before('?')->toString();
-
-    if (! File::exists(__DIR__ . "/../libs/{$file}")) {
-        abort(404);
-    }
-
-    $extension = Str::of($file)->afterLast('.')->toString();
-
-    $type = match ($extension) {
-        'js' => 'application/javascript',
-        'css' => 'text/css',
-        default => 'text/html'
-    };
-
-    return Cache::rememberForever($request->name, function () use ($file, $type, $request) {
-        return response(File::get(__DIR__ . "/../libs/{$file}"))->withHeaders([
-            'Content-Type' => $type,
-        ]);
-    });
 });
--- src/View/Components/Main.php
+++ src/View/Components/Main.php
@@ -4,7 +4,6 @@ namespace Mary\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\View\Component;
 
 class Main extends Component
@@ -20,88 +19,86 @@ class Main extends Component
         public ?string $collapseText = 'Collapse',
         public ?bool $collapsible = false,
     ) {
-        // Add if not exists
-        Cache::add('mary-sidebar-collapsed', 'false');
     }
 
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                 <main @class(["flex mx-auto", "max-w-screen-2xl" => !$fullWidth])>   
-                    
+                 <main @class(["flex mx-auto", "max-w-screen-2xl" => !$fullWidth])>
+
                     <!-- SIDEBAR -->
-                    @if($sidebar)                        
-                        <div @class(["h-screen sticky top-0", "top-20" => $withNav])>                                      
-                            <div>                                                                
-                                <div 
+                    @if($sidebar)
+                        <div @class(["h-screen sticky top-0", "top-20" => $withNav])>
+                            <div>
+                                <div
                                     x-data="
                                         {
-                                            collapsed: {{ cache('mary-sidebar-collapsed') }},
+                                            collapsed: {{ session('mary-sidebar-collapsed', 'false') }},
                                             collapseText: '{{ $collapseText }}',
                                             toggle() {
-                                                this.collapsed = !this.collapsed;     
-                                                fetch('/mary/toogle-sidebar?collapsed=' + this.collapsed); 
+                                                this.collapsed = !this.collapsed;
+                                                fetch('/mary/toogle-sidebar?collapsed=' + this.collapsed);
                                             }
                                         }"
                                     @menu-sub-clicked="if(collapsed) { toggle() }"
                                 >
-                                    <div  
-                                        :class="collapsed 
-                                                    ? '!w-[70px] [&>*_summary::after]:!hidden [&_.mary-hideable]:!hidden [&_.display-when-collapsed]:!block [&_.hidden-when-collapsed]:!hidden' 
-                                                    : '!w-[270px] [&>*_summary::after]:!block [&_.mary-hideable]:!block [&_.hidden-when-collapsed]:!block [&_.display-when-collapsed]:!hidden'"                                        
+                                    <div
+                                        :class="collapsed
+                                                    ? '!w-[70px] [&>*_summary::after]:!hidden [&_.mary-hideable]:!hidden [&_.display-when-collapsed]:!block [&_.hidden-when-collapsed]:!hidden'
+                                                    : '!w-[270px] [&>*_summary::after]:!block [&_.mary-hideable]:!block [&_.hidden-when-collapsed]:!block [&_.display-when-collapsed]:!hidden'"
 
-                                        {{ 
+                                        {{
                                             $sidebar->attributes->class([
                                                 "hidden lg:block h-screen transition-all duration-100 ease-out overflow-y-auto overflow-x-hidden",
                                                 "pb-24" => $withNav,
-                                                "w-[70px] [&>*_summary::after]:hidden [&_.mary-hideable]:hidden [&_.display-when-collapsed]:block [&_.hidden-when-collapsed]:hidden" => cache('mary-sidebar-collapsed') == 'true',
-                                                "w-[270px] [&>*_summary::after]:block [&_.mary-hideable]:block [&_.hidden-when-collapsed]:block [&_.display-when-collapsed]:hidden" => cache('mary-sidebar-collapsed') != 'true'
-                                            ]) 
-                                        }}                                  
-                                    >              
+                                                "w-[70px] [&>*_summary::after]:hidden [&_.mary-hideable]:hidden [&_.display-when-collapsed]:block [&_.hidden-when-collapsed]:hidden" => session('mary-sidebar-collapsed') == 'true',
+                                                "w-[270px] [&>*_summary::after]:block [&_.mary-hideable]:block [&_.hidden-when-collapsed]:block [&_.display-when-collapsed]:hidden" => session('mary-sidebar-collapsed') != 'true'
+                                            ])
+                                        }}
+                                    >
                                         {{ $sidebar }}
 
                                         <!-- SIDEBAR COLLAPSE -->
                                         @if($sidebar->attributes['collapsible'])
                                             <x-menu class="fixed bottom-0 hidden bg-inherit lg:block">
-                                                <x-menu-item 
+                                                <x-menu-item
                                                     @click="toggle"
                                                     icon="o-bars-3-bottom-right"
                                                     title="{{ $sidebar->attributes['collapse-text'] ?? $collapseText }}" />
                                             </x-menu>
                                         @endif
-                                    </div>                    
+                                    </div>
                                 </div>
                             </div>
-                        </div>                        
+                        </div>
                     @endif
 
                     <!-- MAIN CONTENT -->
                     <div {{ $content->attributes->class(["min-h-screen mx-auto w-full p-5 lg:p-10"]) }}>
-                        {{ $content }}                        
-                    </div>              
-                                                                                       
+                        {{ $content }}
+                    </div>
+
                     <!-- DRAWER FOR SIDEBAR -->
-                    @if($sidebar?->attributes['drawer']) 
-                        <x-drawer id="{{ $sidebar->attributes['drawer'] }}"> 
+                    @if($sidebar?->attributes['drawer'])
+                        <x-drawer id="{{ $sidebar->attributes['drawer'] }}">
                             {{ $sidebar }}
-                        </x-drawer>                        
+                        </x-drawer>
                     @endif
-                </main>   
+                </main>
 
                  <!-- FOOTER -->
-                 @if($footer)  
-                    <div 
-                        {{ 
+                 @if($footer)
+                    <div
+                        {{
                             $footer->attributes->class([
                                     "mx-auto w-full",
                                     "max-w-screen-2xl" => !$fullWidth
-                                ]) 
+                                ])
                         }}
                     >
                         {{ $footer }}
                     </div>
-                @endif                    
+                @endif
                 HTML;
     }
 }
