c845df9dd57ba403e95abd2ca3d418fa3d79af17
Robson Tenório <rrtenorio@gmail.com>
2023-11-10 12:02:26 -0300

    Stub: add `csrf` token (#131)

--- stubs/app.blade.php
+++ stubs/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
