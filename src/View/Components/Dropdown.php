cfd54a2197aac06f86109a8bb2145920174bd844
Robson Tenório <rrtenorio@gmail.com>
2023-11-20 21:34:24 -0300

    Dropdown: use Alpine `x-anchor` / MenuItem: improve clickable area (#153)
    * Dropdown/MenuItem: improvements
    
    - Dropdown: use alpine `x-anchor` for better positioning.
    - MenuItem: better clickable area.
    
    * WIP

--- src/View/Components/Dropdown.php
+++ src/View/Components/Dropdown.php
@@ -8,6 +8,8 @@ use Illuminate\View\Component;
 
 class Dropdown extends Component
 {
+    public string $uuid;
+
     public function __construct(
         public ?string $label = null,
         public ?string $icon = 'o-chevron-down',
@@ -16,31 +18,39 @@ class Dropdown extends Component
         //Slots
         public mixed $trigger = null
     ) {
+        $this->uuid = md5(serialize($this));
     }
 
     public function render(): View|Closure|string
     {
         return <<<'HTML'
             <details
-                class="dropdown @if($right) dropdown-end @endif"
                 x-data="{open: false}"
                 @click.outside="open = false"
                 :open="open"
+                class="dropdown"
             >
                 <!-- CUSTOM TRIGGER -->
                 @if($trigger)
-                    <summary @click.prevent="open = !open" class="list-none">
+                    <summary x-ref="button" @click.prevent="open = !open" class="list-none">
                         {{ $trigger }}
                     </summary>
                 @else
                     <!-- DEFAULT TRIGGER -->
-                    <summary @click.prevent="open = !open" {{ $attributes->class(["btn normal-case"]) }}>
+                    <summary x-ref="button" @click.prevent="open = !open" {{ $attributes->class(["btn normal-case"]) }}>
                         {{ $label }}
                         <x-mary-icon :name="$icon" />
                     </summary>
                 @endif
-                <ul @click="open = false" class="dropdown-content p-2 shadow menu z-[1] bg-base-100 dark:bg-base-200 rounded-box whitespace-nowrap">
+
+                <ul
+                    class="p-2 shadow menu z-[1] border border-base-200 bg-base-100 dark:bg-base-200 rounded-box whitespace-nowrap"
+                    @click="open = false"
+                    x-anchor.{{ $right ? 'bottom-end' : 'bottom-start' }}="$refs.button"
+                >
+                    <div wire:key="dropdown-slot-{{ $uuid }}">
                     {{ $slot }}
+                    </div>
                 </ul>
             </details>
         HTML;
