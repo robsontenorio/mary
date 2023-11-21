e438a7efd7eef9dd37f7e972d80b9a575c4f0d09
Robson Tenório <rrtenorio@gmail.com>
2023-11-20 21:34:08 -0300

    Progress: new component (#155)

---
+++ src/View/Components/Loading.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mary\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Loading extends Component
+{
+    public string $uuid;
+
+    public function __construct()
+    {
+        $this->uuid = md5(serialize($this));
+    }
+
+    public function render(): View|Closure|string
+    {
+        return <<<'HTML'
+                <span {{ $attributes->class("loading loading-spinner") }}></span>
+            HTML;
+    }
+}
