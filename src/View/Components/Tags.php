e8645c57c2b5143d3a56482378ae2068374a1f1d
Muntaser Muttaqi <muntasermuttaqi@gmail.com>
2023-11-25 11:03:24 +0600

    use of Mary's internal <x-mary-icon/>

--- src/View/Components/Tags.php
+++ src/View/Components/Tags.php
@@ -25,11 +25,6 @@ class Tags extends Component
     {
         return <<<'HTML'
             <div x-data="{
-                init() {
-                    if (this.tags == null || !Array.isArray(this.tags)) {
-                        this.tags = [];
-                    }
-                },
                 tags: @entangle($attributes->wire('model')),
                 tag: null,
 
@@ -61,10 +56,18 @@ class Tags extends Component
 
                 clearAll() {
                     this.tags = [];
-                }
+                },
+
+                init() {
+                    if (this.tags == null || !Array.isArray(this.tags)) {
+                        this.tags = [];
+                    }
+                },
             }" x-on:keydown.escape="clear()">
-                <!-- LABEL -->
-                <label class="label label-text pt-0 font-semibold" for="{{ $uuid }}">{{ $label }}</label>
+                @if ($label)
+                    <!-- LABEL -->
+                    <label class="label label-text pt-0 font-semibold" for="{{ $uuid }}">{{ $label }}</label>
+                @endif
 
                 <!-- DIV AS INPUT -->
                 <div class="input input-primary relative flex h-fit w-full max-w-full place-items-center justify-between [outline:none!important]">
@@ -72,9 +75,7 @@ class Tags extends Component
                         <template :key="index" x-for="(tag, index) in tags">
                             <div class="my-1 mr-2 inline-block cursor-pointer space-x-2 rounded bg-primary/20 px-2 py-1 transition-all hover:bg-primary/40">
                                 <span x-text="tag"></span>
-                                <svg class="inline-block h-4 w-4 stroke-current transition-all hover:text-red-500" fill="none" viewBox="0 0 24 24" x-on:click="remove(index)" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M6 18L18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
-                                </svg>
+                                <x-mary-icon class="inline-block h-4 w-4 stroke-current transition-all hover:text-red-500" name="o-x-mark" x-on:click="remove(index)" />
                             </div>
                         </template>
 
@@ -82,18 +83,16 @@ class Tags extends Component
                         <input class="input input-bordered input-ghost input-sm my-1 rounded transition-all focus:input-primary" id="{{ $uuid }}" placeholder="{{ $attributes->whereStartsWith('placeholder')->first() }}" type="text" x-model="tag" x-on:click.outside="clear()" x-on:keydown.enter.prevent="push()" x-on:keyup.prevent="if (event.key === ',') { push() }" />
                     </div>
 
-                    <svg class="inline-block h-6 w-6 min-w-fit cursor-pointer stroke-current transition-all hover:text-red-500" fill="none" viewBox="0 0 24 24" x-on:click="clearAll()" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6 18L18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
-                    </svg>
+                    <x-mary-icon class="inline-block h-6 w-6 min-w-fit cursor-pointer stroke-current transition-all hover:text-red-500" name="o-x-mark" x-on:click="clearAll()" x-show="tags.length" />
                 </div>
 
-                <!-- ERROR -->
                 @error($modelName())
+                    <!-- ERROR -->
                     <div class="label-text-alt p-1 text-red-500">{{ $message }}</div>
                 @enderror
 
-                <!-- HINT -->
                 @if ($hint)
+                    <!-- HINT -->
                     <div class="label-text-alt p-1 pb-0 text-gray-400">{{ $hint }}</div>
                 @endif
             </div>
