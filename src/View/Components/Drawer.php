5a9c382c2b51b3830bafa11e6b720af556066e03
Robson Tenório <rrtenorio@gmail.com>
2023-11-27 19:45:52 -0300

    Drawer: fix `wire:model` usage (#166)
    * Header: allow customize `middle` css slot.
    
    * Drawer: fix `wire:model` usage

--- src/View/Components/Drawer.php
+++ src/View/Components/Drawer.php
@@ -20,24 +20,24 @@ class Drawer extends Component
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                @php 
-                    $id = $id ?? $attributes?->whereStartsWith('wire:model')->first() 
+                @php
+                    $id = $id ?? $attributes?->whereStartsWith('wire:model')->first()
                 @endphp
 
                 <div class="drawer absolute z-50 @if($right) drawer-end @endif">
                     <!-- Toggle visibility  -->
-                    <input 
-                        id="{{ $id }}" 
-                        type="checkbox" 
-                        class="drawer-toggle" 
-                        {{ $attributes->whereStartsWith('wire:model') }} />
+                    <input
+                        id="{{ $id }}"
+                        type="checkbox"
+                        class="drawer-toggle"
+                        {{ $attributes->wire('model') }} />
 
                     <div class="drawer-side">
-                        <!-- Overlay effect , click outside -->                        
+                        <!-- Overlay effect , click outside -->
                         <label for="{{ $id }}" class="drawer-overlay"></label>
 
                         <!-- Content -->
-                        <div {{ $attributes->class(['bg-base-100 min-h-screen']) }}>
+                        <div {{ $attributes->except('wire:model')->class(['bg-base-100 min-h-screen']) }}>
                             {{ $slot }}
                         </div>
                     </div>
