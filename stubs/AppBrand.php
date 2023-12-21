598f24ea9c04f51ef215b5e627fcbd6bead00e7f
Robson Tenório <rrtenorio@gmail.com>
2023-12-21 17:42:35 -0300

    Onboarding: better Hello World
    - Adds a nice starter example with working code.

---
+++ stubs/AppBrand.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class AppBrand extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return <<<'blade'
+                <a href="/" wire:navigate>
+                    <!-- Hidden when collapsed -->
+                    <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
+                        <div class="flex items-center gap-2">
+                            <x-icon name="o-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
+                            <span class="font-bold text-3xl mr-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
+                                app
+                            </span>
+                        </div>
+                    </div>
+
+                    <!-- Display when collapsed -->
+                    <div class="display-when-collapsed hidden lg:mx-5 lg:my-6 h-[28px]">
+                        <x-icon name="s-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
+                    </div>
+                </a>
+            blade;
+    }
+}
