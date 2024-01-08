c356e7831f0518d3caba543f92132b657f145f69
Robson Tenório <rrtenorio@gmail.com>
2024-01-08 09:25:55 -0300

    Select: fix array model
    Support models arrays. Ex: wire:model="emails.0" , wire:model="emails.1"

--- src/View/Components/Select.php
+++ src/View/Components/Select.php
@@ -39,6 +39,11 @@ class Select extends Component
     {
         return <<<'HTML'
             <div>
+                 @php
+                     // Wee need this extra step to support models arrays. Ex: wire:model="emails.0"  , wire:model="emails.1"
+                     $uuid = $uuid . $modelName()
+                 @endphp
+            
                 <!-- STANDARD LABEL -->
                 @if($label && !$inline)
                     <label for="{{ $uuid }}" class="pt-0 label label-text font-semibold">
