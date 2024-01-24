1080e960f1ec9d9fcf956a4aaa77bcff96a199ed
Robson Tenório <rrtenorio@gmail.com>
2024-01-24 19:40:39 -0300

    Tab: allow dynamic remove tabs (#275)

--- src/View/Components/Tab.php
+++ src/View/Components/Tab.php
@@ -32,7 +32,15 @@ class Tab extends Component
                     <a
                         class="hidden"
                         :class="{ 'tab-active': selected === '{{ $name }}' }"
-                        x-init="tabs.push({ name: '{{ $name }}', label: {{ json_encode($tabLabel()) }} })"
+                        data-name="{{ $name }}"
+                        x-init="
+                                tabs.push({ name: '{{ $name }}', label: {{ json_encode($tabLabel()) }} });
+                                Livewire.hook('morph.removed', ({el}) => {
+                                    if (el.getAttribute('data-name') == '{{ $name }}'){
+                                        tabs = tabs.filter(i => i.name !== '{{ $name }}')
+                                    }
+                                })
+                            "
                     ></a>
 
                     <div x-show="selected === '{{ $name }}'" role="tabpanel" {{ $attributes->class("tab-content py-5 px-1") }}>
