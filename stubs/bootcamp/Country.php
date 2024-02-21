f8badac8631f3ec24ecdee6fa92794f441e01a86
Robson Tenório <rrtenorio@gmail.com>
2024-02-21 15:07:03 -0300

    WIP

--- stubs/bootcamp/Country.php
+++ stubs/bootcamp/Country.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
     use HasFactory;
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'country_id');
+    }
 }
