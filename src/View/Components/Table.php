619e6d45e6b095efa5e9088ff4f8d41b6a20a2e6
Robson Tenório <rrtenorio@gmail.com>
2024-02-25 22:39:53 -0300

    Table: Improve spacing CSS (#313)
    * Table: CSS with links
    
    Better spacing when links are used.
    
    * WIP

--- src/View/Components/Table.php
+++ src/View/Components/Table.php
@@ -270,7 +270,7 @@ class Table extends Component
                                         @if(isset(${"cell_".$temp_key}))
                                             <td @class([$cellClasses($row, $header), "p-0" => $link])>
                                                 @if($link)
-                                                    <a href="{{ $redirectLink($row) }}" wire:navigate class="block p-4">
+                                                    <a href="{{ $redirectLink($row) }}" wire:navigate class="block py-3 px-4">
                                                 @endif
 
                                                 {{ ${"cell_".$temp_key}($row)  }}
@@ -282,7 +282,7 @@ class Table extends Component
                                         @else
                                             <td @class([$cellClasses($row, $header), "p-0" => $link])>
                                                 @if($link)
-                                                    <a href="{{ $redirectLink($row) }}" wire:navigate class="block p-4">
+                                                    <a href="{{ $redirectLink($row) }}" wire:navigate class="block py-3 px-4">
                                                 @endif
 
                                                 {{ data_get($row, $header['key']) }}
@@ -296,7 +296,7 @@ class Table extends Component
 
                                     <!-- ACTIONS -->
                                     @if($actions)
-                                        <td class="text-right" @click="event.stopPropagation()">{{ $actions($row) }}</td>
+                                        <td class="text-right py-0" @click="event.stopPropagation()">{{ $actions($row) }}</td>
                                     @endif
                                 </tr>
 
