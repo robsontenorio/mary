69f5cc174e9ba537afbc9e026d8c4414d63129eb
Jed Jones <jed@wazoku.com>
2024-03-19 17:28:43 +0000

    Support throwing an exception which produces a toast error

--- src/View/Components/Toast.php
+++ src/View/Components/Toast.php
@@ -47,7 +47,22 @@ class Toast extends Component
                 <script>
                     window.toast = function(payload){
                         window.dispatchEvent(new CustomEvent('mary-toast', {detail: payload}))
-                    }
+                    }        
+                    document.addEventListener('livewire:init', () => {
+                        Livewire.hook('request', ({fail}) => {
+                            fail(({status, content, preventDefault}) => {
+                                let result = JSON.parse(content);
+                
+                                if (result?.toast && typeof window.toast === "function") {
+                                    window.toast(result);
+                                }
+                
+                                if ((result?.prevent_default ?? false) === true) {
+                                    preventDefault();
+                                }
+                            })
+                        })
+                    })
                 </script>
                 @endpersist
             </div>
