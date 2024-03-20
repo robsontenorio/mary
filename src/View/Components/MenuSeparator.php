9adc0355d8519e6a678d770798a18b0d3c5cb48a
REJack <info@rejack.de>
2024-03-20 23:41:58 +0100

    MenuSeparator: use MenuTitle instead of own style

--- src/View/Components/MenuSeparator.php
+++ src/View/Components/MenuSeparator.php
@@ -11,28 +11,20 @@ class MenuSeparator extends Component
     public string $uuid;
 
     public function __construct(
-        public ?string $title = null,
         public ?string $icon = null,
-    ) {
+        public ?string $title = null,
+    )
+    {
         $this->uuid = "mary" . md5(serialize($this));
     }
 
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                <hr class="my-3"/>
+                <div {{ $attributes->class(["divider my-0"]) }}></div>
 
                 @if($title)
-                    <li {{ $attributes->class(["menu-title text-inherit uppercase"]) }}>
-                        <div class="flex items-center gap-2">
-
-                            @if($icon)
-                                <x-mary-icon :name="$icon"  />
-                            @endif
-
-                            {{ $title }}
-                        </div>
-                    </li>
+                    <x-menu-title :title="$title" :icon="$icon"/>
                 @endif
             HTML;
     }
