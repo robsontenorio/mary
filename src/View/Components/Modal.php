180ff173beb77b468de82dfd2683f34194d9f6dd
RealZone22 | Lenny P. <lenny@petschl.org>
2024-03-29 12:26:03 +0100

    Renamed $boxClass to $class

--- src/View/Components/Modal.php
+++ src/View/Components/Modal.php
@@ -12,7 +12,7 @@ class Modal extends Component
         public ?string $id = '',
         public ?string $title = null,
         public ?string $subtitle = null,
-        public ?string $boxClass = null,
+        public ?string $class = null,
         public ?bool $separator = false,
         public ?bool $persistent = false,
 
@@ -39,7 +39,7 @@ class Modal extends Component
                         @endif
                     @endif
                 >
-                    <div class="modal-box {{ $boxClass }}">
+                    <div class="modal-box {{ $class }}">
                         @if($title)
                             <x-mary-header :title="$title" :subtitle="$subtitle" size="text-2xl" :separator="$separator" class="mb-5" />
                         @endif
