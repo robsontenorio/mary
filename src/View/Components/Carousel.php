48d1231f3a7a268c97fa98b341908d7beb43d6f7
CaioCDJ <caiodjesus1@hotmail.com>
2024-06-19 14:55:43 -0300

    Add the native daisy carousel component

---
+++ src/View/Components/Carousel.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Mary\View\Components;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class Carousel extends Component
+{
+    public string $uuid;
+
+    public function __construct(
+        public array $images,
+        public ?bool $withArrows = false,
+        public ?bool $withIndicators = false,
+    ) {
+        $this->uuid = "mary" . md5(serialize($this));
+    }
+
+    public function render(): View|Closure|string
+    {
+        return <<<'HTML'
+            <div>
+                @if($withArrows)
+                    <div
+                        {{ $attributes->class(['carousel w-full']) }}
+                        wire:key="{{ $uuid }}">
+
+                        @for($i=0; $i < count($images); $i++)
+
+                            <div id="slide{{$i}}" class="carousel-item relative w-full justify-center">
+                                <img src="{!! $images[$i] !!}" class="w-full h-full" />
+                                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                    <a
+                                        href="#slide{{($i==0)?$i:$i-1}}"
+                                        class="btn btn-circle">❮</a>
+                                    <a
+                                        href="#slide{{($i==count($images))?$i:$i+1}}"
+                                        class="btn btn-circle">❯</a>
+                                </div>
+                            </div>
+                        @endfor
+                    </div>
+
+                @else
+                    <div
+                        {{ $attributes->class(['carousel rounded-box']) }}
+                        wire:key="{{ $uuid }}">
+
+                        @if($images)
+                            @for($i=0; $i < count($images); $i++)
+                                <div id='image{!!$i!!}' class="carousel-item">
+                                    <img  src="{!! $images[$i] !!}"  />
+                                </div>
+                            @endfor
+                        @endif
+
+                    </div>
+                    @if($withIndicators)
+                        <div class="flex justify-center w-full py-2 gap-2">
+                            @for($i=0; $i < count($images); $i++)
+                                <a href='#image{!!$i!!}' class="btn btn-xs">{{$i+1}}</a>
+                            @endfor
+                        </div>
+                    @endif
+                @endif
+            </div>
+        HTML;
+    }
+}
