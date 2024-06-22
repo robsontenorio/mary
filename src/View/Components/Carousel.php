72d8a5367107aa9097f4cec7662bc55267b91c63
Robson Tenório <rrtenorio@gmail.com>
2024-06-21 21:28:05 -0300

    WIP

--- src/View/Components/Carousel.php
+++ src/View/Components/Carousel.php
@@ -6,71 +6,126 @@ use Closure;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
+/**
+ * Inspired by Penguin UI.
+ * Thank you.
+ */
 class Carousel extends Component
 {
     public string $uuid;
-    public bool $associative = false;
 
     public function __construct(
-        public array $images,
-        public ?bool $withArrows = false,
-        public ?bool $withIndicators = false,
+        public array $slides,
+        public ?bool $withoutIndicators = false,
+        public ?bool $withoutArrows = false,
+
+        // Slots
+        public mixed $content = null,
     ) {
         $this->uuid = "mary" . md5(serialize($this));
-        $this->associative =  gettype($this->images[0])=="array"
-        ? true
-        : false;
     }
 
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-            <div>
-                @if($withArrows)
-                    <div
-                        {{ $attributes->class(['carousel w-full']) }}
-                        wire:key="{{ $uuid }}">
+            <div x-data="{
+                slides: @js($slides),
+                withoutIndicators: {{ json_encode($withoutIndicators) }},
+                currentSlideIndex: 1,
+                touchStartX: null,
+                touchEndX: null,
+                swipeThreshold: 50,
+                previous() {
+                    this.currentSlideIndex = (this.currentSlideIndex > 1)
+                        ? --this.currentSlideIndex
+                        : this.slides.length
+                },
+                next() {
+                    this.currentSlideIndex = (this.currentSlideIndex < this.slides.length)
+                        ? ++this.currentSlideIndex
+                        : 1
+                },
+                handleTouchStart(event) {
+                    this.touchStartX = event.touches[0].clientX
+                },
+                handleTouchMove(event) {
+                    this.touchEndX = event.touches[0].clientX
+                },
+                handleTouchEnd() {
+                    if(this.touchEndX){
+                        if (this.touchStartX - this.touchEndX > this.swipeThreshold) {
+                            this.next()
+                        }
+                        if (this.touchStartX - this.touchEndX < -this.swipeThreshold) {
+                            this.previous()
+                        }
+                        this.touchStartX = null
+                        this.touchEndX = null
+                    }
+                },
+            }" class="relative w-full overflow-hidden">
 
-                        @for($i=0; $i < count($images); $i++)
+                @if(!$withoutArrows)
+                    <!-- previous button -->
+                    <x-mary-button icon="o-chevron-left"  @click="previous()" class="absolute cursor-pointer left-5 top-1/2 z-[2] btn-circle btn-sm" />
+                    <!-- next button -->
+                    <x-mary-button icon="o-chevron-right"  @click="next()" class="absolute cursor-pointer right-5 top-1/2 z-[2] btn-circle btn-sm" />
+                @endif
 
-                            <div id="slide{{$i}}" class="carousel-item relative w-full justify-center">
-                                <a @if($associative) href="{!! $images[$i]['link'] !!}" @endif >
-                                    <img src="{!! $associative ? $images[$i]['image']: $images[$i] !!}" class="w-full h-full" />
-                                </a>
-                                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a
-                                        href="#slide{{($i==0)?$i:$i-1}}"
-                                        class="btn btn-circle">❮</a>
-                                    <a
-                                        href="#slide{{($i==count($images))?$i:$i+1}}"
-                                        class="btn btn-circle">❯</a>
+                <!-- slides -->
+                <div
+                     @touchstart="handleTouchStart($event)" @touchmove="handleTouchMove($event)" @touchend="handleTouchEnd()"
+                    {{ $attributes->class(["relative h-64 w-full rounded-box overflow-hidden"]) }}
+                >
+                    <!-- Slot content -->
+                    @foreach($slides as $index => $slide)
+                        <div
+                            x-cloak
+                            x-show="currentSlideIndex == {{ $index + 1 }}"
+                            x-transition.opacity.duration.500ms
+                            @class(["absolute inset-0", "cursor-pointer" => data_get($slide, 'url') ])
+                            @if(data_get($slide, 'url'))
+                                @click="window.location = '{{ data_get($slide, 'url') }}'"
+                            @endif
+                        >
+                            <!-- Custom content -->
+                            @if($content)
+                                <div class="absolute inset-0 z-[1]">
+                                     {{ $content($slide) }}
                                 </div>
-                            </div>
-                        @endfor
-                </div>
+                            <!-- Default content -->
+                            @else
+                                  <div
+                                      @class([
+                                          "absolute inset-0 z-[1] flex flex-col items-center justify-end gap-2 px-20 py-12 text-center",
+                                           "bg-gradient-to-t from-slate-900/85" => data_get($slide, 'urlText') || data_get($slide, 'title') || data_get($slide, 'description')
+                                      ])
+                                >
+                                    <!-- Title -->
+                                    <h3 class="w-full text-2xl lg:text-3xl font-bold text-white">{{ data_get($slide, 'title') }}</h3>
 
-                @else
-                    <div
-                        {{ $attributes->class(['carousel rounded-box']) }}
-                        wire:key="{{ $uuid }}">
-                        @if($images)
-                            @for($i=0; $i < count($images); $i++)
-                                <div id='image{!!$i!!}' class="carousel-item">
-                                    <a @if($associative) href="{!! $images[$i]['link'] !!}" @endif>
-                                        <img  src="{!! $associative ? $images[$i]['image']: $images[$i] !!}"  />
-                                    </a>
+                                    <!-- Description -->
+                                    <div class="w-full text-sm text-white mb-5">{{ data_get($slide, 'description') }}</div>
+
+                                    <!-- Button-->
+                                    @if(data_get($slide, 'urlText'))
+                                        <a href="{{ data_get($slide, 'url') }}" class="btn btn-sm btn-ghost my-3 border-white text-white hover:border-white hover:scale-105">{{ data_get($slide, 'urlText') }}</a>
+                                    @endif
                                 </div>
-                            @endfor
-                        @endif
+                            @endif
 
-                    </div>
-                    @if($withIndicators)
-                        <div class="flex justify-center w-full py-2 gap-2">
-                            @for($i=0; $i < count($images); $i++)
-                                <a href='#image{!!$i!!}' class="btn btn-xs">{{$i+1}}</a>
-                            @endfor
+                            <!-- Image -->
+                            <img class="w-full h-full inset-0 object-cover" src="{{ data_get($slide, 'image') }}" />
                         </div>
-                    @endif
+                    @endforeach
+                </div>
+                <!-- indicators -->
+                @if(! $withoutIndicators)
+                    <div class="absolute rounded-xl bottom-3 md:bottom-5 left-1/2 z-[2] flex -translate-x-1/2 gap-4 md:gap-3 bg-white/75 px-1.5 py-1 md:px-2 dark:bg-slate-900/75" role="group" aria-label="slides" >
+                        <template x-for="(slide, index) in slides">
+                            <button class="size-2.5 cursor-pointer rounded-full transition hover:scale-125 bg-slate-700 dark:bg-slate-300" @click="currentSlideIndex = index + 1" :class="[currentSlideIndex === index + 1 ? 'bg-slate-700 dark:bg-slate-300' : 'bg-slate-700/50 dark:bg-slate-300/50']"></button>
+                        </template>
+                    </div>
                 @endif
             </div>
         HTML;
