0be686047c8159c83fbe76ad95ef20d06c77feb6
Andreas Feichtinger <anfeichtingers@gmail.com>
2024-07-28 08:09:46 +0200

    Textarea: fix missmatch between id and label

--- src/View/Components/Textarea.php
+++ src/View/Components/Textarea.php
@@ -37,6 +37,11 @@ class Textarea extends Component
     {
         return <<<'HTML'
             <div>
+                @php
+                    // Wee need this extra step to support models arrays. Ex: wire:model="emails.0"  , wire:model="emails.1"
+                    $uuid = $uuid . $modelName()
+                @endphp
+
                 <!-- STANDARD LABEL -->
                 @if($label && !$inline)
                     <label for="{{ $uuid }}" class="pt-0 label label-text font-semibold">
@@ -53,13 +58,12 @@ class Textarea extends Component
                 <div class="flex-1 relative">
                     <!-- INPUT -->
                     <textarea
+                        id="{{ $uuid }}"
                         placeholder = "{{ $attributes->whereStartsWith('placeholder')->first() }} "
 
                         {{
                             $attributes
-                            ->merge([
-                                'id' => $uuid
-                            ])
+                            ->merge([])
                             ->class([
                                 'textarea textarea-primary w-full peer',
                                 'pt-5' => ($inline && $label),
