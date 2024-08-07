92ba895cf6ba943120ebe8e2e0cec30ed6271a07
Robson Tenório <rrtenorio@gmail.com>
2024-08-06 22:48:39 -0300

    WIP

--- src/View/Components/Textarea.php
+++ src/View/Components/Textarea.php
@@ -62,9 +62,9 @@ class Textarea extends Component
 
                         {{
                             $attributes
-                             ->merge([
-                                 'id' => $uuid
-                             ])
+                            ->merge([
+                                'id' => $uuid
+                            ])
                             ->class([
                                 'textarea textarea-primary w-full peer',
                                 'pt-5' => ($inline && $label),
