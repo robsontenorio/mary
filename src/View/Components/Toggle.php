cb6a289b2c394ed52423e6c07e01198911dcf382
Andreas Feichtinger <anfeichtingers@gmail.com>
2024-09-14 08:02:17 +0200

    Toggle: add required symbol if given, add validation errors

--- src/View/Components/Toggle.php
+++ src/View/Components/Toggle.php
@@ -14,9 +14,25 @@ class Toggle extends Component
         public ?string $label = null,
         public ?string $hint = null,
         public ?bool $right = false,
-        public ?bool $tight = false
+        public ?bool $tight = false,
+        public ?string $hintClass = 'label-text-alt text-gray-400 py-1 pb-0',
+        // Validations
+        public ?string $errorField = null,
+        public ?string $errorClass = 'text-red-500 label-text-alt p-1',
+        public ?bool $omitError = false,
+        public ?bool $firstErrorOnly = false,
     ) {
         $this->uuid = "mary" . md5(serialize($this));
+    }
+
+    public function modelName(): ?string
+    {
+        return $this->attributes->whereStartsWith('wire:model')->first();
+    }
+
+    public function errorFieldName(): ?string
+    {
+        return $this->errorField ?? $this->modelName();
     }
 
     public function render(): View|Closure|string
@@ -28,6 +44,10 @@ class Toggle extends Component
                         @if($right)
                             <span @class(["flex-1" => !$tight])>
                                 {{ $label}}
+                                
+                                @if($attributes->get('required'))
+                                    <span class="text-error">*</span>
+                                @endif
                             </span>
                         @endif
 
@@ -35,12 +55,27 @@ class Toggle extends Component
 
                         @if(!$right)
                             {{ $label}}
+                            
+                            @if($attributes->get('required'))
+                                <span class="text-error">*</span>
+                            @endif
                         @endif
                     </label>
 
+                    <!-- ERROR -->
+                    @if(!$omitError && $errors->has($errorFieldName()))
+                        @foreach($errors->get($errorFieldName()) as $message)
+                            @foreach(Arr::wrap($message) as $line)
+                                <div class="{{ $errorClass }}" x-classes="text-red-500 label-text-alt p-1">{{ $line }}</div>
+                                @break($firstErrorOnly)
+                            @endforeach
+                            @break($firstErrorOnly)
+                        @endforeach
+                    @endif
+
                     <!-- HINT -->
                     @if($hint)
-                        <div class="label-text-alt text-gray-400 py-1 pb-0">{{ $hint }}</div>
+                        <div class="{{ $hintClass }}" x-classes="label-text-alt text-gray-400 py-1 pb-0">{{ $hint }}</div>
                     @endif
                 </div>
         HTML;
