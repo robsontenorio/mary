f10d5dc459d68d938d3c1952d18c34751a7df8c4
Robson Tenório <rrtenorio@gmail.com>
2024-09-16 20:21:15 -0300

    Declare `$__bladeCompiler`

--- src/MaryServiceProvider.php
+++ src/MaryServiceProvider.php
@@ -215,7 +215,7 @@ class MaryServiceProvider extends ServiceProvider
              */
             $name = str_replace('.', '___', $name);
 
-            return "<?php \$loop = null; \$__env->slot({$name}, function({$functionArguments}) use ({$uses}) { \$loop = (object) \$__env->getLoopStack()[0] ?>";
+            return "<?php \$__bladeCompiler = \$__bladeCompiler ?? null; \$loop = null; \$__env->slot({$name}, function({$functionArguments}) use ({$uses}) { \$loop = (object) \$__env->getLoopStack()[0] ?>";
         });
 
         Blade::directive('endscope', function () {
