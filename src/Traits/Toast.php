0a272134c3e9656ba6e7eda67ee6721f90b0dbec
Robson Tenório <rrtenorio@gmail.com>
2025-01-01 11:12:50 -0300

    Toast: fix PHP 8.4 deprecation warning (#750)

--- src/Traits/Toast.php
+++ src/Traits/Toast.php
@@ -9,24 +9,24 @@ trait Toast
     public function toast(
         string $type,
         string $title,
-        string $description = null,
-        string $position = null,
+        ?string $description = null,
+        ?string $position = null,
         string $icon = 'o-information-circle',
         string $css = 'alert-info',
         int $timeout = 3000,
-        string $redirectTo = null
+        ?string $redirectTo = null
     ) {
         $toast = [
             'type' => $type,
             'title' => $title,
             'description' => $description,
             'position' => $position,
-            'icon' => Blade::render("<x-mary-icon class='w-7 h-7' name='" . $icon . "' />"),
+            'icon' => Blade::render("<x-mary-icon class='w-7 h-7' name='".$icon."' />"),
             'css' => $css,
             'timeout' => $timeout,
         ];
 
-        $this->js('toast(' . json_encode(['toast' => $toast]) . ')');
+        $this->js('toast('.json_encode(['toast' => $toast]).')');
 
         session()->flash('mary.toast.title', $title);
         session()->flash('mary.toast.description', $description);
@@ -38,48 +38,48 @@ trait Toast
 
     public function success(
         string $title,
-        string $description = null,
-        string $position = null,
+        ?string $description = null,
+        ?string $position = null,
         string $icon = 'o-check-circle',
         string $css = 'alert-success',
         int $timeout = 3000,
-        string $redirectTo = null
+        ?string $redirectTo = null
     ) {
         return $this->toast('success', $title, $description, $position, $icon, $css, $timeout, $redirectTo);
     }
 
     public function warning(
         string $title,
-        string $description = null,
-        string $position = null,
+        ?string $description = null,
+        ?string $position = null,
         string $icon = 'o-exclamation-triangle',
         string $css = 'alert-warning',
         int $timeout = 3000,
-        string $redirectTo = null
+        ?string $redirectTo = null
     ) {
         return $this->toast('warning', $title, $description, $position, $icon, $css, $timeout, $redirectTo);
     }
 
     public function error(
         string $title,
-        string $description = null,
-        string $position = null,
+        ?string $description = null,
+        ?string $position = null,
         string $icon = 'o-x-circle',
         string $css = 'alert-error',
         int $timeout = 3000,
-        string $redirectTo = null
+        ?string $redirectTo = null
     ) {
         return $this->toast('error', $title, $description, $position, $icon, $css, $timeout, $redirectTo);
     }
 
     public function info(
         string $title,
-        string $description = null,
-        string $position = null,
+        ?string $description = null,
+        ?string $position = null,
         string $icon = 'o-information-circle',
         string $css = 'alert-info',
         int $timeout = 3000,
-        string $redirectTo = null
+        ?string $redirectTo = null
     ) {
         return $this->toast('info', $title, $description, $position, $icon, $css, $timeout, $redirectTo);
     }
