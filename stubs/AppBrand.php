7e8179d621cc1363c0e0400303bf3376be2db67f
Robson Tenório <rrtenorio@gmail.com>
2025-03-16 18:54:32 -0300

    WIP

--- stubs/AppBrand.php
+++ stubs/AppBrand.php
@@ -26,7 +26,7 @@ class AppBrand extends Component
                     <!-- Hidden when collapsed -->
                     <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
                         <div class="flex items-center gap-2">
-                            <x-icon name="o-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
+                            <x-icon name="o-cube" class="w-6 -mb-1.5 text-purple-500" />
                             <span class="font-bold text-3xl me-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
                                 app
                             </span>
@@ -34,8 +34,8 @@ class AppBrand extends Component
                     </div>
 
                     <!-- Display when collapsed -->
-                    <div class="display-when-collapsed hidden mx-5 mt-4 lg:mb-6 h-[28px]">
-                        <x-icon name="s-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
+                    <div class="display-when-collapsed hidden mx-5 mt-5 mb-1 h-[28px]">
+                        <x-icon name="s-cube" class="w-6 -mb-1.5 text-purple-500" />
                     </div>
                 </a>
             HTML;
