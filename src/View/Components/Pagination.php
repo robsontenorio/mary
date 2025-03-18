95bc4be18e5fd10795376188186dfe38821dfb19
Robson Tenório <rrtenorio@gmail.com>
2025-03-17 23:18:52 -0300

    WIP

--- src/View/Components/Pagination.php
+++ src/View/Components/Pagination.php
@@ -36,9 +36,9 @@ class Pagination extends Component
                 <div {{ $attributes->class(["mb-4 border-t border-t-base-content/10"]) }}></div>
                 <div class="justify-between md:flex md:flex-row w-auto md:w-full items-center overflow-y-auto pl-2 pr-2 relative">
                     @if($isShowable())
-                    <div class="flex flex-row justify-center md:justify-start mb-2 md:mb-0">
+                    <div class="flex flex-row justify-center md:justify-start mb-2 md:mb-0 py-1">
                         <select id="{{ $uuid }}" @if(!empty($modelName())) wire:model.live="{{ $modelName() }}" @endif
-                                class="select select-primary select-sm flex sm:text-sm sm:leading-6 w-auto md:mr-5">
+                                class="select select-sm flex sm:text-sm sm:leading-6 w-auto md:mr-5">
                             @foreach ($perPageValues as $option)
                             <option value="{{ $option }}" @selected($rows->perPage() === $option)>{{ $option }}</option>
                             @endforeach
