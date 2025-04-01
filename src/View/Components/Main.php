5ab594fbf1768b81547da3bdcde50035b3e215dd
Robson Tenório <rrtenorio@gmail.com>
2025-04-01 02:21:14 -0300

    Main: fix height (#832)
    Close #825

--- src/View/Components/Main.php
+++ src/View/Components/Main.php
@@ -54,7 +54,7 @@ class Main extends Component
                                 }"
 
                                 @menu-sub-clicked="if(collapsed) { toggle() }"
-                                @class(["drawer-side z-20 lg:z-auto", "top-0 lg:top-[73px] lg:h-[calc(100vh-73px)]" => $withNav])
+                                @class(["drawer-side z-20 lg:z-auto", "top-0 lg:top-[65px] lg:h-[calc(100vh-65px)]" => $withNav])
                             >
                                 <label for="{{ $sidebar?->attributes['drawer'] }}" aria-label="close sidebar" class="drawer-overlay"></label>
 
@@ -69,7 +69,7 @@ class Main extends Component
                                             "flex flex-col !transition-all !duration-100 ease-out overflow-x-hidden overflow-y-auto h-screen",
                                             "w-[62px] [&>*_summary::after]:hidden [&_.mary-hideable]:hidden [&_.display-when-collapsed]:block [&_.hidden-when-collapsed]:hidden" => session('mary-sidebar-collapsed') == 'true',
                                             "w-[270px] [&>*_summary::after]:block [&_.mary-hideable]:block [&_.hidden-when-collapsed]:block [&_.display-when-collapsed]:hidden" => session('mary-sidebar-collapsed') != 'true',
-                                            "lg:h-[calc(100vh-73px)]" => $withNav
+                                            "lg:h-[calc(100vh-65px)]" => $withNav
                                         ])
                                      }}
                                 >
