5b25542cb2d9719d5d209930f9f0e1afd5cec68b
Robson Tenório <rrtenorio@gmail.com>
2025-04-03 18:33:38 -0300

    WIP

--- src/View/Components/Breadcrumbs.php
+++ src/View/Components/Breadcrumbs.php
@@ -11,23 +11,35 @@ class Breadcrumbs extends Component
     public string $uuid;
 
     /**
-     * @param array  $items  The steps that should be displayed. Each element supports the keys 'label', 'link', 'icon' and 'tooltip'.
-     * @param string  $seperator  Any supported icon name, 'o-slash' by default.
+     * @param  array  $items  The steps that should be displayed. Each element supports the keys 'label', 'link', 'icon' and 'tooltip'.
+     * @param  string  $separator  Any supported icon name, 'o-slash' by default.
      * @param ?string  $linkItemClass  The classes that should be applied to each item with a link.
      * @param ?string  $textItemClass  The classes that should be applied to each item without a link.
      * @param ?string  $iconClass  The classes that should be applied to each items icon.
-     * @param ?string  $seperatorClass  The classes that should be applied to each seperator.
+     * @param ?string  $separatorClass  The classes that should be applied to each separator.
      */
     public function __construct(
         public ?string $id = null,
         public array $items = [],
-        public string $seperator = 'o-slash',
-        public ?string $linkItemClass = "btn btn-ghost btn-sm",
-        public ?string $textItemClass = "btn btn-ghost btn-sm pointer-events-none",
-        public ?string $iconClass = "h-5 w-5",
-        public ?string $seperatorClass = "h-4 w-4",
+        public string $separator = 'o-chevron-right',
+        public ?string $linkItemClass = "hover:underline text-sm",
+        public ?string $textItemClass = "text-sm",
+        public ?string $iconClass = "h-4 w-4",
+        public ?string $separatorClass = "h-3 w-3 mx-1 text-base-content/40",
     ) {
         $this->uuid = "mary" . md5(serialize($this)) . $id;
+    }
+
+    public function tooltip(array $element): ?string
+    {
+        return $element['tooltip'] ?? $element['tooltip-left'] ?? $element['tooltip-right'] ?? $element['tooltip-bottom'] ?? $element['tooltip-top'] ?? null;
+    }
+
+    public function tooltipPosition(array $element): string
+    {
+        return array_key_exists('tooltip-left', $element) ? 'lg:tooltip-left' :
+            (array_key_exists('tooltip-right', $element) ? 'lg:tooltip-right' :
+                (array_key_exists('tooltip-bottom', $element) ? 'lg:tooltip-bottom' : 'lg:tooltip-top'));
     }
 
     public function render(): View|Closure|string
@@ -35,15 +47,13 @@ class Breadcrumbs extends Component
         return <<<'BLADE'
                 <ul {{ $attributes->merge(['class' => 'flex items-center']) }} wire:key="{{ $uuid }}">
                     @foreach($items as $element)
-                        
+
                         {{-- Tooltip --}}
-                        @php
-                            $tooltip = $element['tooltip'] ?? $element['tooltip-left'] ?? $element['tooltip-right'] ?? $element['tooltip-bottom'] ?? $element['tooltip-top'] ?? null;
-                            $tooltipPosition = array_key_exists('tooltip-left', $element) ? 'lg:tooltip-left' : (array_key_exists('tooltip-right', $element) ? 'lg:tooltip-right' : (array_key_exists('tooltip-bottom', $element) ? 'lg:tooltip-bottom' : 'lg:tooltip-top'));
-                        @endphp
-                        <li @class(["!inline-flex lg:tooltip $tooltipPosition" => $tooltip]) 
-                            @if($tooltip)
-                                data-tip="{{ $tooltip }}"
+                        <li
+                            @class(["lg:tooltip {$tooltipPosition($element)}" => $tooltip($element), "hidden sm:block" => !$loop->first && !$loop->last])
+
+                            @if($tooltip($element))
+                                data-tip="{{ $tooltip($element) }}"
                             @endif
                         >
 
@@ -55,27 +65,34 @@ class Breadcrumbs extends Component
 
                                 {{-- Icon --}}
                                 @if($element['icon'] ?? null)
-                                    <x-mary-icon :name="$element['icon']" @class([$iconClass]) />
+                                    <x-mary-icon :name="$element['icon']" @class(["mb-0.5", $iconClass]) />
                                 @endif
 
                                 {{-- Text --}}
                                 <span>
                                     {{ $element['label'] ?? null }}
                                 </span>
-                                
+
                             @if ($element['link'] ?? null)
                                 </a>
                             @else
                                 </span>
                             @endif
-
                         </li>
-                        
-                        {{-- Seperator --}}
-                        @if (!$loop->last)
-                            <x-mary-icon name="{{ $seperator }}" @class([$seperatorClass]) />
+
+                        @if($loop->remaining == 1 && $loop->count > 2)
+                            <span class="sm:hidden">...</span>
                         @endif
 
+                        {{-- Separator --}}
+                        <span @class([
+                                "hidden",
+                                "!block" => ($loop->first || $loop->remaining == 1) && $loop->count > 1,
+                                "sm:!block" => !$loop->last && $loop->count > 1
+                             ])
+                        >
+                            <x-mary-icon :name="$separator" @class([$separatorClass]) />
+                        </span>
                     @endforeach
                 </ul>
             BLADE;
