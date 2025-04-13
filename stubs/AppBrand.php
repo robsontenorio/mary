568f502a921b4fb49b4c0cdcc2ccacdeb1d127c0
Robson Tenório <rrtenorio@gmail.com>
2025-04-13 20:06:38 -0300

    Update AppBrand.php

--- stubs/AppBrand.php
+++ stubs/AppBrand.php
@@ -25,7 +25,7 @@ class AppBrand extends Component
                 <a href="/" wire:navigate>
                     <!-- Hidden when collapsed -->
                     <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
-                        <div class="flex items-center gap-2">
+                        <div class="flex items-center gap-2 w-fit">
                             <x-icon name="o-cube" class="w-6 -mb-1.5 text-purple-500" />
                             <span class="font-bold text-3xl me-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
                                 app
