b5f8facf33b54f75fe7a4bf37ccc0322da2aad97
Timur Abduraimov <t.abduraimov@matriks.group>
2025-04-17 11:09:06 +0300

    fix: add dispatchChangeEvent call to selectAll method of Choices component

--- src/View/Components/Choices.php
+++ src/View/Components/Choices.php
@@ -146,6 +146,7 @@ class Choices extends Component
                             },
                             selectAll() {
                                 this.selection = this.options.map(i => i.{{ $optionValue }})
+                                this.dispatchChangeEvent({ value: this.selection})
                             },
                             clear() {
                                 this.focused = false;
