f9bcb0fd1378911c21304b478918b9bfca3ffb43
Lucas Chevalot <159005604+lchevalot@users.noreply.github.com>
2025-04-28 19:49:03 +0200

    Choices/ChoicesOffline: fix select height for styling classes and vertical stretching (#873)
    * Fix Choices component min and max height (#853)
    
    * Fix ChoicesOffline component height + change Choices min height for select content (#853)
    
    ---------
    
    Co-authored-by: lchevalot <lucas@itcpiercing.com>

--- src/View/Components/Choices.php
+++ src/View/Components/Choices.php
@@ -260,7 +260,7 @@ class Choices extends Component
 
                                         {{
                                             $attributes->whereStartsWith('class')->class([
-                                                "select w-full h-fit pl-2.5",
+                                                "select w-full min-h-fit pl-2.5",
                                                 "join-item" => $prepend || $append,
                                                 "border-dashed" => $attributes->has("readonly") && $attributes->get("readonly") == true,
                                                 "!select-error" => $errorFieldName() && $errors->has($errorFieldName()) && !$omitError
@@ -277,7 +277,7 @@ class Choices extends Component
                                             <x-mary-icon :name="$icon" class="pointer-events-none w-4 h-4 opacity-40" />
                                         @endif
 
-                                        <div class="w-full py-0.5 min-h-9.5 content-center text-wrap">
+                                        <div class="w-full py-0.5 min-h-3 content-center text-wrap">
 
                                             {{-- SELECTED OPTIONS --}}
                                             <span wire:key="selected-options-{{ $uuid }}">
