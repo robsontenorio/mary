72e85d2cbf26691450ecc804a5969f41e25248a4
Robson Tenório <rrtenorio@gmail.com>
2025-04-28 15:47:22 -0300

    File: Set it `w-full` (#884)
    Set `w-full` as the default to ensure consistent styling across all input components.

--- src/View/Components/File.php
+++ src/View/Components/File.php
@@ -167,7 +167,7 @@ class File extends Component
 
                             {{
                                 $attributes->whereDoesntStartWith('class')->class([
-                                    "file-input",
+                                    "file-input w-full",
                                     "!file-input-error" => $errorFieldName() && $errors->has($errorFieldName()) && !$omitError,
                                     "hidden" => $slot->isNotEmpty()
                                 ])
