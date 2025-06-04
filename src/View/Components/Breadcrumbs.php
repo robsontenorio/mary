28b8fb19a2e1b1fa1b23b222c95a83f2bf197be0
Robson Tenório <rrtenorio@gmail.com>
2025-06-04 12:03:24 -0300

    Breadcrumbs: support `wire:navigate`
    Fix: #922

--- src/View/Components/Breadcrumbs.php
+++ src/View/Components/Breadcrumbs.php
@@ -17,6 +17,7 @@ class Breadcrumbs extends Component
      * @param ?string  $textItemClass  The classes that should be applied to each item without a link.
      * @param ?string  $iconClass  The classes that should be applied to each items icon.
      * @param ?string  $separatorClass  The classes that should be applied to each separator.
+     * @param ?bool  $noWireNavigate  If true, the component will not use wire:navigate on links.
      */
     public function __construct(
         public ?string $id = null,
@@ -26,6 +27,7 @@ class Breadcrumbs extends Component
         public ?string $textItemClass = "text-sm",
         public ?string $iconClass = "h-4 w-4",
         public ?string $separatorClass = "h-3 w-3 mx-1 text-base-content/40",
+        public ?bool $noWireNavigate = false,
     ) {
         $this->uuid = "mary" . md5(serialize($this)) . $id;
     }
@@ -61,7 +63,7 @@ class Breadcrumbs extends Component
                         >
 
                             @if ($element['link'] ?? null)
-                                <a href="{{ $element['link'] }}" @class([$linkItemClass])>
+                                <a href="{{ $element['link'] }}" @if(!$noWireNavigate) wire:navigate @endif @class([$linkItemClass])>
                             @else
                                 <span @class([$textItemClass])>
                             @endif
