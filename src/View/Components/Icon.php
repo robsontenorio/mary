f48b5138976d015b91284aee79b06038df9635fa
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-06-24 08:55:01 -0400

    Icon: apply click events to labels (#935)
    * add the possibility to icon attr receive the attributes with or without label
    
    * WIP
    
    ---------
    
    Co-authored-by: Robson Tenório <rrtenorio@gmail.com>

--- src/View/Components/Icon.php
+++ src/View/Components/Icon.php
@@ -39,11 +39,13 @@ class Icon extends Component
                 @if(strlen($label ?? '') > 0)
                     <div class="inline-flex items-center gap-1">
                 @endif
-                    <x-svg :name="$icon()" {{ $attributes->class(['inline', 'w-5 h-5' => !Str::contains($attributes->get('class') ?? '', ['w-', 'h-']) ]) }}
+                    <x-svg
+                        :name="$icon()"
+                        {{ $attributes->class(['inline', 'w-5 h-5' => !Str::contains($attributes->get('class') ?? '', ['w-', 'h-']) ]) }}
                     />
 
                 @if(strlen($label ?? '') > 0)
-                        <div class="{{ $labelClasses() }}">
+                        <div class="{{ $labelClasses() }}" {{ $attributes->whereStartsWith('@') }}>
                             {{ $label }}
                         </div>
                     </div>
