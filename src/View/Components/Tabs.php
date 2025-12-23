42eea1909548741fdf4834528aa4c1505616dc29
Robson Tenório <rrtenorio@gmail.com>
2025-12-23 17:20:34 -0300

    Tabs: remove old workaround
    Remove old workaround, needed in previous versions of Livewire/Alpine.
    
    Currently, back browser navigation breaks tabs.

--- src/View/Components/Tabs.php
+++ src/View/Components/Tabs.php
@@ -33,13 +33,6 @@ class Tabs extends Component
                                     @else
                                         @entangle($attributes->wire('model'))
                                     @endif
-                                 ,
-                                 init() {
-                                     // Fix weird issue when navigating back
-                                     document.addEventListener('livewire:navigating', () => {
-                                         document.querySelectorAll('.tab').forEach(el =>  el.remove());
-                                     });
-                                 }
                         }"
                         class="{{ $tabsClass }}"
                         x-class="font-semibold pb-1 border-b-[length:var(--border)] border-b-base-content/50 border-b-base-content/10 flex overflow-x-auto scrollbar-hide relative w-full"
