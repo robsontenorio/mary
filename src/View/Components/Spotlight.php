8c593664466ce6f7be696d29d4cadaf0c0bec4c0
Robson Tenório <rrtenorio@gmail.com>
2025-12-30 16:15:49 -0300

    Fix template syntax in Spotlight component

--- src/View/Components/Spotlight.php
+++ src/View/Components/Spotlight.php
@@ -171,7 +171,7 @@ class Spotlight extends Component
 
                             <!-- RESULTS  -->
                             <div class="-mx-1 mt-1" @click="close()" @keydown.enter="close()" x-ref="spotResults">
-                                <template x-for="(item, index) in results" :key="index" >
+                                <template x-for="(item, index) in results" :key="index">
                                     <!-- ITEM -->
                                     <a x-bind:href="item.link" class="mary-spotlight-element" @if(!$noWireNavigate) wire:navigate @endif tabindex="0">
                                         <div class="p-3 hover:bg-base-200 border-t-[length:var(--border)] border-t-base-content/10" >
