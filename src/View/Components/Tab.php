cafa202d2a8d2c21ccc010d46290a225b529b79c
Robson Tenório <rrtenorio@gmail.com>
2026-07-07 13:19:21 -0300

    WIP

--- src/View/Components/Tab.php
+++ src/View/Components/Tab.php
@@ -4,56 +4,74 @@ namespace Mary\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Component;
 
-class Tabs extends Component
+class Tab extends Component
 {
     public string $uuid;
 
     public function __construct(
         public ?string $id = null,
-        public ?string $selected = null,
-        public string $labelClass = 'font-semibold pb-1',
-        public string $activeClass = 'border-b-[length:var(--border)] border-b-base-content/50',
-        public string $labelDivClass = 'border-b-[length:var(--border)] border-b-base-content/10 flex overflow-x-auto',
-        public string $tabsClass = 'relative w-full',
+        public ?string $name = null,
+        public ?string $label = null,
+        public ?string $icon = null,
+        public bool $disabled = false,
+        public bool $hidden = false,
     ) {
         $this->uuid = "mary" . md5(serialize($this)) . $id;
+    }
+
+    public function tabLabel(string $label): string
+    {
+        $fromLabel = $this->label ? $this->label : $label;
+
+        if ($this->icon) {
+            return Blade::render("
+                <x-mary-icon name='" . $this->icon . "' @class([
+                'me-2',
+                'whitespace-nowrap',
+                'text-base-content/30 cursor-not-allowed' => '$this->disabled'
+                ])>
+                    <x-slot:label>
+                        {$fromLabel}
+                    </x-slot:label>
+                </x-mary-icon>
+            ");
+        }
+
+        return Blade::render("
+            <div @class([
+                'whitespace-nowrap',
+                'text-base-content/30 cursor-not-allowed' => '$this->disabled'
+                ])>
+                {$fromLabel}
+            </div>
+        ");
     }
 
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                    <div
-                        x-data="{
-                                tabs: [],
-                                selected:
-                                    @if($selected)
-                                        '{{ $selected }}'
-                                    @else
-                                        @entangle($attributes->wire('model'))
-                                    @endif
-                        }"
-                        class="{{ $tabsClass }}"
-                        x-class="font-semibold pb-1 border-b-[length:var(--border)] border-b-base-content/50 border-b-base-content/10 flex overflow-x-auto scrollbar-hide relative w-full"
-                    >
-                        <!-- TAB LABELS -->
-                        <div class="{{ $labelDivClass }}">
-                            <template x-for="tab in tabs" :key="tab.name">
-                                <a
-                                    role="tab"
-                                    x-init="if (typeof tab == 'undefined') $el.remove()"
-                                    x-html="tab.label"
-                                     @click="tab.disabled ? null: selected = tab.name"
-                                    :class="{ '{{ $activeClass }} tab-active': selected === tab.name, 'hidden': tab.hidden }"
-                                    class="tab {{ $labelClass }}"></a>
-                            </template>
-                        </div>
+                    <a
+                        class="hidden tab"
+                        :class="{ 'tab-active': selected === '{{ $name }}' }"
+                        data-name="{{ $name }}"
+                        x-init="
+                                const newItem = { name: '{{ $name }}', label: {{ json_encode($tabLabel($label)) }}, disabled: {{ $disabled ? 'true' : 'false' }}, hidden: {{ $hidden ? 'true' : 'false' }} };
+                                const index = tabs.findIndex(item => item.name === '{{ $name }}');
+                                index !== -1 ? tabs[index] = newItem : tabs.push(newItem);
 
-                        <!-- TAB CONTENT -->
-                        <div role="tablist" {{ $attributes->except(['wire:model', 'wire:model.live'])->class(["block"]) }}>
-                            {{ $slot }}
-                        </div>
+                                Livewire.hook('morph.removed', ({el}) => {
+                                    if (el.getAttribute('data-name') == '{{ $name }}'){
+                                        tabs = tabs.filter(i => i.name !== '{{ $name }}')
+                                    }
+                                })
+                            "
+                    ></a>
+
+                    <div x-show="selected === '{{ $name }}'" role="tabpanel" {{ $attributes->class("tab-content py-5 px-1") }}>
+                        {{ $slot }}
                     </div>
                 HTML;
     }
