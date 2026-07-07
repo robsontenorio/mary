6d19a92abf1011b30d253d19a832b92562afcb67
Robson Tenório <rrtenorio@gmail.com>
2026-07-07 13:20:06 -0300

    Revert wrong commit (#1095)
    * Revert
    
    * WIP
    
    * WIP

--- src/View/Components/Tab.php
+++ src/View/Components/Tab.php
@@ -4,6 +4,7 @@ namespace Mary\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\View\Component;
 
 class Tab extends Component
@@ -17,45 +18,60 @@ class Tab extends Component
         public ?string $icon = null,
         public bool $disabled = false,
         public bool $hidden = false,
-        public ?string $badge = null,
-        public ?string $badgeClass = null,
     ) {
         $this->uuid = "mary" . md5(serialize($this)) . $id;
+    }
+
+    public function tabLabel(string $label): string
+    {
+        $fromLabel = $this->label ? $this->label : $label;
+
+        if ($this->icon) {
+            return Blade::render("
+                <x-mary-icon name='" . $this->icon . "' @class([
+                'me-2',
+                'whitespace-nowrap',
+                'text-base-content/30 cursor-not-allowed' => '$this->disabled'
+                ])>
+                    <x-slot:label>
+                        {$fromLabel}
+                    </x-slot:label>
+                </x-mary-icon>
+            ");
+        }
+
+        return Blade::render("
+            <div @class([
+                'whitespace-nowrap',
+                'text-base-content/30 cursor-not-allowed' => '$this->disabled'
+                ])>
+                {$fromLabel}
+            </div>
+        ");
     }
 
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                    @aware(['lift', 'box', 'labelClass', 'contentClass', 'activeClass'])
+                    <a
+                        class="hidden tab"
+                        :class="{ 'tab-active': selected === '{{ $name }}' }"
+                        data-name="{{ $name }}"
+                        x-init="
+                                const newItem = { name: '{{ $name }}', label: {{ json_encode($tabLabel($label)) }}, disabled: {{ $disabled ? 'true' : 'false' }}, hidden: {{ $hidden ? 'true' : 'false' }} };
+                                const index = tabs.findIndex(item => item.name === '{{ $name }}');
+                                index !== -1 ? tabs[index] = newItem : tabs.push(newItem);
 
-                    <div>
-                        <label
-                            @class([
-                                "tab flex gap-3 flex-nowrap whitespace-nowrap px-5",
-                                $labelClass,
-                                "hidden" => $hidden,
-                                "tab-disabled" => $disabled
-                             ])
-                            :class="{ '{{ $activeClass }}': selected === '{{ $name }}' }"
-                        >
-                            <input id="{{ ($id ?? $uuid).$name }}" type="radio" name="{{ ($id ?? $uuid).$name }}" value="{{ $name }}" x-model="selected" />
+                                Livewire.hook('morph.removed', ({el}) => {
+                                    if (el.getAttribute('data-name') == '{{ $name }}'){
+                                        tabs = tabs.filter(i => i.name !== '{{ $name }}')
+                                    }
+                                })
+                            "
+                    ></a>
 
-                            @if($icon)
-                              <x-mary-icon :name="$icon"  />
-                            @endif
-
-                            {{ $label }}
-
-                            @if ($badge)
-                                <x-badge :value="$badge" @class(["badge-sm badge-primary", $badgeClass]) />
-                            @endif
-                        </label>
-                        <div
-                            x-show="selected == '{{ $name }}'"
-                            {{ $attributes->class(["tab-content bg-base-100 py-5 px-3 border-t-base-300 block rounded-none", $contentClass, "px-5 border-base-300 rounded!" => $lift || $box]) }}
-                         >
-                            {{ $slot }}
-                        </div>
+                    <div x-show="selected === '{{ $name }}'" role="tabpanel" {{ $attributes->class("tab-content py-5 px-1") }}>
+                        {{ $slot }}
                     </div>
                 HTML;
     }
