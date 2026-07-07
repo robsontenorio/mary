87cedd59aebb72b641dade907530be48cf8941c2
Robson Tenório <rrtenorio@gmail.com>
2026-07-07 13:17:30 -0300

    Revert

--- src/View/Components/Tab.php
+++ src/View/Components/Tab.php
@@ -6,19 +6,17 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Tab extends Component
+class Tabs extends Component
 {
     public string $uuid;
 
     public function __construct(
         public ?string $id = null,
-        public ?string $name = null,
-        public ?string $label = null,
-        public ?string $icon = null,
-        public bool $disabled = false,
-        public bool $hidden = false,
-        public ?string $badge = null,
-        public ?string $badgeClass = null,
+        public ?string $selected = null,
+        public string $labelClass = 'font-semibold pb-1',
+        public string $activeClass = 'border-b-[length:var(--border)] border-b-base-content/50',
+        public string $labelDivClass = 'border-b-[length:var(--border)] border-b-base-content/10 flex overflow-x-auto',
+        public string $tabsClass = 'relative w-full',
     ) {
         $this->uuid = "mary" . md5(serialize($this)) . $id;
     }
@@ -26,34 +24,34 @@ class Tab extends Component
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                    @aware(['lift', 'box', 'labelClass', 'contentClass', 'activeClass'])
+                    <div
+                        x-data="{
+                                tabs: [],
+                                selected:
+                                    @if($selected)
+                                        '{{ $selected }}'
+                                    @else
+                                        @entangle($attributes->wire('model'))
+                                    @endif
+                        }"
+                        class="{{ $tabsClass }}"
+                        x-class="font-semibold pb-1 border-b-[length:var(--border)] border-b-base-content/50 border-b-base-content/10 flex overflow-x-auto scrollbar-hide relative w-full"
+                    >
+                        <!-- TAB LABELS -->
+                        <div class="{{ $labelDivClass }}">
+                            <template x-for="tab in tabs" :key="tab.name">
+                                <a
+                                    role="tab"
+                                    x-init="if (typeof tab == 'undefined') $el.remove()"
+                                    x-html="tab.label"
+                                     @click="tab.disabled ? null: selected = tab.name"
+                                    :class="{ '{{ $activeClass }} tab-active': selected === tab.name, 'hidden': tab.hidden }"
+                                    class="tab {{ $labelClass }}"></a>
+                            </template>
+                        </div>
 
-                    <div>
-                        <label
-                            @class([
-                                "tab flex gap-3 flex-nowrap whitespace-nowrap px-5",
-                                $labelClass,
-                                "hidden" => $hidden,
-                                "tab-disabled" => $disabled
-                             ])
-                            :class="{ '{{ $activeClass }}': selected === '{{ $name }}' }"
-                        >
-                            <input id="{{ ($id ?? $uuid).$name }}" type="radio" name="{{ ($id ?? $uuid).$name }}" value="{{ $name }}" x-model="selected" />
-
-                            @if($icon)
-                              <x-mary-icon :name="$icon"  />
-                            @endif
-
-                            {{ $label }}
-
-                            @if ($badge)
-                                <x-badge :value="$badge" @class(["badge-sm badge-primary", $badgeClass]) />
-                            @endif
-                        </label>
-                        <div
-                            x-show="selected == '{{ $name }}'"
-                            {{ $attributes->class(["tab-content bg-base-100 py-5 px-3 border-t-base-300 block rounded-none", $contentClass, "px-5 border-base-300 rounded!" => $lift || $box]) }}
-                         >
+                        <!-- TAB CONTENT -->
+                        <div role="tablist" {{ $attributes->except(['wire:model', 'wire:model.live'])->class(["block"]) }}>
                             {{ $slot }}
                         </div>
                     </div>
